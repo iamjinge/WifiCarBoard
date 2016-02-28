e66c5fc82bd8138cee83b75e4f398834f5e4705f
a1994846931931 <a1994846931931@foxmail.com>
2016-02-28 18:21:26 +0800

    tiny change

--- ModeFreeMain/ModeFree.h
+++ ModeFreeMain/ModeFree.h
@@ -6,10 +6,10 @@ int detectLeft = 22;    //左侧红外避障模块检测
 int detectRight = 23;  //右侧红外避障模块检测
 int detectMid = 24;    //中间红外避障模块检测
 
-const int forwardPWM = 150;
-const int backwardPWM = 150;
-const int turnLeftPWM = 150;
-const int turnRightPWM = 150;
+int forwardPWM = 150;
+int backwardPWM = 150;
+int turnLeftPWM = 150;
+int turnRightPWM = 150;
 
 void modeSetup() {
   pinMode(detectLeft, INPUT);
@@ -27,25 +27,26 @@ void modeLoop() {
     Motion::turnLeft(turnLeftPWM, 1500, true);
   }
   else if (!left && !right) {
+    
     Motion::forward(forwardPWM);
     //    Motion::forward(forwardPWM,1000);
-    Serial.println("1");
+//    Serial.println("1");
   }
   else if (left && right) {
     Motion::backward(backwardPWM, 500, true);
     //int randomAngle=random()
     Motion::turnLeft(turnLeftPWM, 1500, true);
-    Serial.println("2");
+//    Serial.println("2");
   }
   else if (left) {
     Motion::backward(backwardPWM, 200);
     Motion::turnRight(turnRightPWM, 1000, true);
-    Serial.println("3");
+//    Serial.println("3");
   }
   else {
     Motion::backward(backwardPWM, 200);
     Motion::turnLeft(turnLeftPWM, 1000, true);
-    Serial.println("4");
+//    Serial.println("4");
   }
   //  Serial.print(left);
   //  Serial.print(' ');
