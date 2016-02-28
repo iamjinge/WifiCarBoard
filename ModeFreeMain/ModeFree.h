4606eb031c0a3f8e66034d129c36c41702c25ac8
a1994846931931 <a1994846931931@foxmail.com>
2016-02-28 18:16:29 +0800

    Add switch

--- ModeFreeMain/ModeFree.h
+++ ModeFreeMain/ModeFree.h
@@ -2,31 +2,53 @@
 #include"Motion.h"
 
 namespace ModeFree {
-bool detectLeft = 22;  //左侧红外避障模块检测
-bool detectRight = 23;  //右侧红外避障模块检测
-const static int forwardPWM = 150;
-const static int backwardPWM = 150;
-const static int turnLeftPWM = 150;
-const static int turnRightPWM = 150;
+int detectLeft = 22;    //左侧红外避障模块检测
+int detectRight = 23;  //右侧红外避障模块检测
+int detectMid = 24;    //中间红外避障模块检测
+
+const int forwardPWM = 150;
+const int backwardPWM = 150;
+const int turnLeftPWM = 150;
+const int turnRightPWM = 150;
+
+void modeSetup() {
+  pinMode(detectLeft, INPUT);
+  pinMode(detectRight, INPUT);
+  pinMode(detectMid, INPUT);
+}
 
 void modeLoop() {
-  bool left = ~digitalRead(detectLeft);
-  bool right = ~digitalRead(detectRight);
-  if (~left && ~right) {
+  bool mid = !digitalRead(detectMid);
+  bool left = !digitalRead(detectLeft);
+  bool right = !digitalRead(detectRight);
+  if (mid) {
+    Motion::backward(backwardPWM, 500, true);
+    //int randomAngle=random()
+    Motion::turnLeft(turnLeftPWM, 1500, true);
+  }
+  else if (!left && !right) {
     Motion::forward(forwardPWM);
+    //    Motion::forward(forwardPWM,1000);
+    Serial.println("1");
   }
   else if (left && right) {
     Motion::backward(backwardPWM, 500, true);
     //int randomAngle=random()
     Motion::turnLeft(turnLeftPWM, 1500, true);
+    Serial.println("2");
   }
   else if (left) {
     Motion::backward(backwardPWM, 200);
     Motion::turnRight(turnRightPWM, 1000, true);
+    Serial.println("3");
   }
   else {
     Motion::backward(backwardPWM, 200);
     Motion::turnLeft(turnLeftPWM, 1000, true);
+    Serial.println("4");
   }
+  //  Serial.print(left);
+  //  Serial.print(' ');
+  //  Serial.println(right);
 }
 };
--- ModeFreeMain/ModeFreeMain.ino
+++ ModeFreeMain/ModeFreeMain.ino
@@ -1,15 +1,28 @@
 #include "ModeFree.h"
-int mode=1;      //避障模式
+
+int switchPin = 25;
+int mode = 1;    //避障模式
 void setup() {
   // put your setup code here, to run once:
   Serial3.begin(9600);
+  Serial.begin(9600);
+  pinMode(switchPin, INPUT_PULLUP);
   Motion::motionSetup();
+  ModeFree::modeSetup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  switch(mode){
-    case 1:
-    ModeFree::modeLoop();
+  //  Serial.println(digitalRead(switchPin));
+  if (!digitalRead(switchPin)) {
+    Serial.println(digitalRead(switchPin));
+    switch (mode) {
+      case 1:
+        ModeFree::modeLoop();
+    }
   }
+  else {
+    Motion::motionStop();
+  }
+  //Serial.println(digitalRead(22));
 }
