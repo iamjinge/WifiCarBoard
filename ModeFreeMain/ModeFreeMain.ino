afc14fcb94fd53d747ce6904b23f095141009f7e
a1994846931931 <a1994846931931@foxmail.com>
2016-02-29 12:20:37 +0800

    delete
    Serial3 not used in this code

--- ModeFreeMain/ModeFreeMain.ino
+++ ModeFreeMain/ModeFreeMain.ino
@@ -4,7 +4,7 @@ int switchPin = 25;
 int mode = 1;    //避障模式
 void setup() {
   // put your setup code here, to run once:
-  Serial3.begin(9600);
+  //  Serial3.begin(9600); //Wifi串口
   Serial.begin(9600);
   pinMode(switchPin, INPUT_PULLUP);
   Motion::motionSetup();
@@ -15,7 +15,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   //  Serial.println(digitalRead(switchPin));
   if (!digitalRead(switchPin)) {
-    Serial.println(digitalRead(switchPin));
+    //    Serial.println(digitalRead(switchPin));
     switch (mode) {
       case 1:
         ModeFree::modeLoop();
